f247cd981b3fcb033f304c621afb3ff759620365
Ganesh <ganeshsuryawanshi1306@gmail.com>
2025-07-12 16:48:16 +0530

    My Files

--- src/Oct/ex_02102024/Lab_001.java
+++ src/Oct/ex_02102024/Lab_001.java
@@ -1,4 +1,11 @@
 package Oct.ex_02102024;
 
 public class Lab_001 {
+    public static void main(String[] args) {
+        System.out.println("hello Java");
+        /* This is Multiline comment
+        *This is used to documentation,refrence
+        * Auther - Ganesh
+        */
+    }
 }
--- src/Oct/ex_04102024_Main_Method/Lab_003.java
+++ src/Oct/ex_04102024_Main_Method/Lab_003.java
@@ -1,4 +1,12 @@
 package Oct.ex_04102024_Main_Method;
 
 public class Lab_003 {
+    public static void main(String[] args) {
+        // Print table of 10
+        System.out.println("10*1 = 10");
+        System.out.println("10*2 = 20");
+        System.out.println("10*3 = 30");
+        System.out.println("10*4 = 40");
+        System.out.println("10*5 = 50");
+    }
 }

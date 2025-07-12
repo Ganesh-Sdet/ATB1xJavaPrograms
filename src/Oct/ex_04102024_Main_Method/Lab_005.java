1461ad65926994f00aa65f4cccef50d6595f707f
Ganesh <ganeshsuryawanshi1306@gmail.com>
2025-07-12 17:00:15 +0530

    My Files

---
+++ src/Oct/ex_04102024_Main_Method/Lab_005.java
@@ -0,0 +1,9 @@
+package Oct.ex_04102024_Main_Method;
+
+public class Lab_005 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println(a+b);
+    }
+}

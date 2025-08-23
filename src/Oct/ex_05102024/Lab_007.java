3b8b6bf68aaa834be9639e38850a4ac3b6b84bad
Ganesh <ganeshsuryawanshi1306@gmail.com>
2025-08-23 16:21:42 +0530

    My Files

---
+++ src/Oct/ex_05102024/Lab_007.java
@@ -0,0 +1,13 @@
+package Oct.ex_05102024;
+
+public class Lab_007 {
+
+    public static void main (String[] args){
+        int age = 65;
+        System.out.println(age);
+        // int -> data type = - whole numbers (not decimals)
+        // age ->  variable_name / identifier
+        // = -> assignment operator
+        // 65 -> literal / variable_value
+    }
+}

79c11fc314ee6322979587199a5e73004beba7aa
Ganesh <ganeshsuryawanshi1306@gmail.com>
2025-10-12 22:12:07 +0530

    My Files

---
+++ src/Oct/ex_11102024_Operators/Lab041.java
@@ -0,0 +1,16 @@
+package Oct.ex_11102024_Operators;
+
+public class Lab041 {
+    public static void main(String[] args) {
+        // Unary Operator
+        int a = 65; // + ->  unary operator
+        int b = -1; // - ->  unary operator
+        System.out.println(a);
+        System.out.println(b);
+        b = b+1;
+        System.out.println(b);
+
+        a = a+1;
+        System.out.println(a);
+    }
+}
